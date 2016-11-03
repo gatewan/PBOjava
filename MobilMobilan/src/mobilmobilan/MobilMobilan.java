6be6d47b9f88b272bb28f09037d061b998eea8b9
gatewan <gatewawan@ymail.com>
2016-11-04 00:48:15 +0700

    Deklarasi Variabel Warna dan Tahun Produksi

--- MobilMobilan/src/mobilmobilan/MobilMobilan.java
+++ MobilMobilan/src/mobilmobilan/MobilMobilan.java
@@ -16,6 +16,8 @@ public class MobilMobilan {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String warna;
+        int tahunProduksi;   
     }
     
 }
